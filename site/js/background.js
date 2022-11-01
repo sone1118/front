047097946e5a68a6e12b02d7714071dbd2d93705
Jenos <sone1118@naver.com>
2022-11-01 23:03:23 +0900

    배경 이미지 변경

--- site/js/background.js
+++ site/js/background.js
@@ -1,4 +1,4 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
 const image = images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url(./img/${image})`;
